565a80aa1fd1532a4d8463fa662051bfbf9dd3c0
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-01 17:13:12 +0300

    it will add the user registration form class

--- igclone/forms.py
+++ igclone/forms.py
@@ -10,4 +10,14 @@ from emoji_picker.widgets import EmojiPickerTextInputAdmin, EmojiPickerTextareaA
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name', max_length=50)
     email = forms.EmailField(label='Email')
-    
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1','password1']
+        help_texts = {'username':None, 'password2':None}
+
+User._meta.get_field('email')._unique=True
+
