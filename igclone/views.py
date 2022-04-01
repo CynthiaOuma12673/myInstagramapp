be85e3d728efb2824542b2189ecced53bc8e1d2a
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-01 17:30:37 +0300

    it will add the imports to the views

--- igclone/views.py
+++ igclone/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
-
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Follow, Image,Profile,Comments
+from django.contrib.auth.models import User
+from .forms import NewsLetterForm, UpdateUserForm, UpdateUserProfileForm, UserRegisterForm,PostForm,CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
 # Create your views here.
 
